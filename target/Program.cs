ae0c803159a4e6c0c0e4f220f6279e6174f19c45
Gabriel Pereira <ghoppvp2407@gmail.com>
2024-09-03 22:07:01 -0300

    verificação de fibonacci, verificação da letra A e questoes 3, 4 e 5

---
+++ target/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using target.src;
+
+namespace target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.menuPrograma();
+
+            Console.ReadKey();
+        }
+    }
+}
+
